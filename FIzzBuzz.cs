bdba7ee5d4e11f35ba2e0342d34d35525fe475ee
Eryk <erykcesarek256@gmail.com>
2021-04-11 15:31:18 +0200

    get it done

---
+++ FIzzBuzz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        private static string FizzBuzz(int x)
+        {
+            if (x % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (x % 5 == 0)
+            {
+                return "Buzz";
+            }
+            if (x % 3 == 0)
+            {
+                return "Fizz";
+            }
+            return x.ToString();
+        }
+        static void Main(string[] args)
+        {
+            Enumerable.Range(1, 100).Select(x => FizzBuzz(x)).ToList().ForEach(x =>
+            {
+                Console.WriteLine(x);
+            });
+
+        }
+    }
+}
